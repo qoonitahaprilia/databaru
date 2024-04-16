0dc235880a443375b3ef5fab63087b29a7930013
qoonitahaprilia <167208131+qoonitahaprilia@users.noreply.github.com>
2024-04-17 06:59:26 +0700

    Add files via upload

---
+++ views/about_tampil.php
@@ -0,0 +1,41 @@
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="panel panel-success">
+                <div class="panel-heading">
+                    <h3 class="panel-title"><span class="fa fa-user-plus"></span> Tentang</h3>
+                </div>
+                <div class="panel-body">
+                    <table id="deskripsi" class="table table-bordered table-striped table-hover">
+                        <thead>
+                            <p align="center"><img src="img/aries.png"></img></p>
+                            <label class="col-sm-12 control-label"><center><strong>SISTEM INFORMASI PERPUSTAKAAN <br> ARIES</strong></center></label>
+                        </thead>
+                        <tbody>
+                          <div class="col-sm-2" align="center">
+
+                          </div>
+                          <div class="col-sm-8" align="center">
+                            <br>
+                            Sistem Informasi Perpustakaan Aries
+                            <br>
+                            <br>
+                            Universitas Diponegoro
+                            <br>
+                            Nama : Qoonitah Aprilia
+                            <br>
+                            NIM  : 12030122140225
+                            <br>
+                            Kelas: Pengkodean dan Pemrograman E
+                            <br>
+                          </div>
+                          <div class="col-sm-2" align="center">
+                          </div>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
---
+++ views/kontak_tampil.php
@@ -0,0 +1,38 @@
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="panel panel-success">
+                <div class="panel-heading">
+                    <h3 class="panel-title"><span class="fa fa-user-plus"></span> Kontak</h3>
+                </div>
+                <div class="panel-body">
+                    <table id="deskripsi" class="table table-bordered table-striped table-hover">
+                        <thead>
+                            <label class="col-sm-12 control-label"><center><strong>PERPUSTAKAAN ARIES</strong></center></label>
+                            <p align="center">
+                              Jalan Mahoni, Sula Barat, Kisaran Barat<br>
+                              Kabupaten Manggala, Sumatera Barat, Kode Pos : 201313
+                            </p>
+                        </thead>
+                        <tbody>
+                          <div class="col-sm-2" align="center">
+
+                          </div>
+                          <div class="col-sm-8" align="center">
+                            <br>
+                            Telp. 085781428798<br>
+                            Email : qoonitahaprilia02@gmail.com<br>
+                            Fax : 12563113<br>
+                            Website : perpusaries.go.id<br>
+                            <br>
+                          </div>
+                          <div class="col-sm-2" align="center">
+                          </div>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
