e4df1fbc57b7ad98fcf8af1e27f782f27cd17948
qoonitahaprilia <167208131+qoonitahaprilia@users.noreply.github.com>
2024-04-17 13:51:11 +0700

    Add files via upload

---
+++ beranda_adm.php
@@ -0,0 +1,37 @@
+<div class="container">
+    <div class="row">
+        <div class ="col-xs-12">
+
+            <div class="alert alert-info">
+                Selamat datang kembali <strong><?=$_SESSION['nama']?></strong>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <!--colomn kedua-->
+        <div class="col-sm-12 col-xs-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Halaman Administrator Sistem Informasi Perpustakaan Aries</h3>
+                </div>
+                <div class="panel-body">
+                     <table id="deskripsi" class="table table-bordered table-striped table-hover">
+                       <thead>
+                          <p align="center"><img src="img/aries.png"></img></p>
+                          <p class="label-title" align="center"><strong>PERPUSTAKAAN ARIES</strong></p>
+                          <p class="col-sm-12 col-xs-12" align="center">
+                            Halo, Selamat datang di halaman administrator Sistem Informasi Perpustakaan Aries.<br>
+                            Melalui halaman ini, anda dapat melakukan pengelolaan data Buku Perpustakaan, pengelolaan peminjaman Buku Perpustakaan, <br>
+                            dan juga pengelolaan laporan Buku serta peminjamannya.<br><br>
+
+
+                          </p>
+
+                         </thead
+
+
+                    </table>
+                </div>
+            </div>
+        </div>
+</div>
